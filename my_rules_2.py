1ff826b0e133ac3da429066374d5fb829bdf627b
ivanalgarbe <147286505+ivanalgarbe@users.noreply.github.com>
2024-02-28 23:49:31 -0500

    Add files via upload

--- my_rules_2.py
+++ my_rules_2.py
@@ -29,3 +29,13 @@ ifile = open(input_file, "r")
 for line in ifile:
     print(line)
 
+# Function to generate frequent 1-itemsets
+def generate_frequent_1_itemsets(input_file):
+    freq_itemsets = {}
+    with open(input_file, "r") as ifile:
+        for line in ifile:
+            transaction_id, item_id = map(int, line.strip().split())
+            freq_itemsets[item_id] = freq_itemsets.get(item_id, 0) + 1
+    return freq_itemsets
+
+
