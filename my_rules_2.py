510fc01ae7d937cfbd2405b809284d9cea41e923
fnotorious <fnato001@fiu.edu>
2024-03-01 19:56:47 -0500

    Removed massive comment block

--- my_rules_2.py
+++ my_rules_2.py
@@ -9,26 +9,6 @@ if len(argv) != 5:
 minsuppc, minconf, input_file, output_file = argv[1:5]
 freq_itemsets, cand_itemsets = {}, {}
 
-# Way below is a code snippet that iterates over every line in the input_file and prints it
-# You can use it to generate the frequent itemsets. Inside the small.txt file, every line in 
-# is organized like this:
-# 
-# 0 10
-# 0 21
-# ..
-# transaction_id item_id
-#
-# Since we're doing only F1 itemsets, for every line you iterate in the txt file, just do this:
-#
-# freq_itemsets[item_id] = freq_itemsets.get(item_id, 0) + 1
-#
-# Every time the F1 itemset shows up in the transaction, it adds 1 to it in the hash. This keeps
-# track of the itemset's frequency.
-
-ifile = open(input_file, "r")
-for line in ifile:
-    print(line)
-
 # Function to generate frequent 1-itemsets
 def generate_frequent_1_itemsets(input_file):
     freq_itemsets = {}
@@ -37,5 +17,3 @@ def generate_frequent_1_itemsets(input_file):
             transaction_id, item_id = map(int, line.strip().split())
             freq_itemsets[item_id] = freq_itemsets.get(item_id, 0) + 1
     return freq_itemsets
-
-
